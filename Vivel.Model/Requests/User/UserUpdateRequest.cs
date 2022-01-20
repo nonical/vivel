5aef0a02cbf4a3425f586f226379c3b81274bad1
Nedžad Alibegović <nedzad@nedzad.dev>
2022-01-21 00:15:15 +0100

    Add Required attribute back

--- Vivel.Model/Requests/User/UserUpdateRequest.cs
+++ Vivel.Model/Requests/User/UserUpdateRequest.cs
@@ -7,6 +7,7 @@ namespace Vivel.Model.Requests.User
 {
     public class UserUpdateRequest
     {
+        [Required]
         [JsonConverter(typeof(SmartEnumNameConverter<BloodType, int>))]
         public BloodType BloodType { get; set; }
         [Required]
