3eb9d18c0c68702fbe8def01dcc9d0f31863177b
Harun Sabljaković <harun.sabljakovic@edu.fit.ba>
2022-01-24 20:58:13 +0100

    Add api service
    add generic api service to communicate with external api
    add object extension to parse request object to query string
    add settings

---
+++ Vivel.Desktop/Services/APIService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+using Vivel.Model.Extensions;
+
+namespace Vivel.Desktop.Services
+{
+    public class APIService
+    {
+        private readonly string _apiUrl;
+
+        public APIService(string controllerName)
+        {
+            _apiUrl = $"{Properties.Settings.Default.ApiURL}/{controllerName}";
+        }
+        public async Task<T> Get<T>(object request)
+        {
+            var queryString = await request.ToQueryString();
+
+            var result = await $"{_apiUrl}?{queryString}".GetJsonAsync<T>();
+
+            return result;
+        }
+
+        public async Task<T> GetByID<T>(string id)
+        {
+            var result = await $"{_apiUrl}/{id}".GetJsonAsync<T>();
+
+            return result;
+        }
+
+        public async Task<T> Insert<T>(object request)
+        {
+            var result = await $"{_apiUrl}".PostJsonAsync(request).ReceiveJson<T>();
+
+            return result;
+        }
+
+        public async Task<T> Update<T>(string id, object request)
+        {
+            var result = await $"{_apiUrl}/{id}".PutJsonAsync(request).ReceiveJson<T>();
+
+            return result;
+        }
+    }
+}
