ff9936d3151e4f33c389bbed5dbfcb41c4f83442
Harun Sabljaković <harun.sabljakovic@edu.fit.ba>
2022-01-25 11:09:07 +0100

    Hsab/setup winforms (#23)
    * setup windows forms app
    
    * add flurl package
    
    * downgrade net standard to 2.0
    
    it was impossible to include Vivel.Model project as a reference to Vivel.Desktop so we had to downgrade it to make it possible
    
    * Add api service
    
    add generic api service to communicate with external api
    add object extension to parse request object to query string
    add settings
    
    * add parent form with tabs
    
    * Update .dotnet.yml
    
    Co-authored-by: Nedžad Alibegović <nedzad@nedzad.dev>

---
+++ Vivel.Desktop/frmMain.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Vivel.Desktop
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.hospitalsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drivesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fAQToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.presetBadgesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            //
+            // menuStrip
+            //
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hospitalsToolStripMenuItem,
+            this.usersToolStripMenuItem,
+            this.drivesToolStripMenuItem,
+            this.fAQToolStripMenuItem,
+            this.presetBadgesToolStripMenuItem,
+            this.reportsToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(632, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "MenuStrip";
+            //
+            // hospitalsToolStripMenuItem
+            //
+            this.hospitalsToolStripMenuItem.Name = "hospitalsToolStripMenuItem";
+            this.hospitalsToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.hospitalsToolStripMenuItem.Text = "Hospitals";
+            //
+            // usersToolStripMenuItem
+            //
+            this.usersToolStripMenuItem.Name = "usersToolStripMenuItem";
+            this.usersToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.usersToolStripMenuItem.Text = "Users";
+            //
+            // drivesToolStripMenuItem
+            //
+            this.drivesToolStripMenuItem.Name = "drivesToolStripMenuItem";
+            this.drivesToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.drivesToolStripMenuItem.Text = "Drives";
+            //
+            // fAQToolStripMenuItem
+            //
+            this.fAQToolStripMenuItem.Name = "fAQToolStripMenuItem";
+            this.fAQToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.fAQToolStripMenuItem.Text = "FAQ";
+            //
+            // reportsToolStripMenuItem
+            //
+            this.reportsToolStripMenuItem.Name = "reportsToolStripMenuItem";
+            this.reportsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.reportsToolStripMenuItem.Text = "Reports";
+            //
+            // presetBadgesToolStripMenuItem
+            //
+            this.presetBadgesToolStripMenuItem.Name = "presetBadgesToolStripMenuItem";
+            this.presetBadgesToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
+            this.presetBadgesToolStripMenuItem.Text = "Preset Badges";
+            //
+            // frmMain
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 453);
+            this.Controls.Add(this.menuStrip);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "frmMain";
+            this.Text = "frmMain";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem hospitalsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem drivesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fAQToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem presetBadgesToolStripMenuItem;
+    }
+}
