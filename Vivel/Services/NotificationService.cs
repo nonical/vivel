3ee4ab3aab04613fd8717e97700f40b9b7fef275
Harun Sabljaković <harun.sabljakovic@edu.fit.ba>
2022-02-21 22:34:21 +0100

    implement postnotifications method

--- Vivel/Services/NotificationService.cs
+++ Vivel/Services/NotificationService.cs
@@ -17,5 +17,21 @@ namespace Vivel.Services
         public NotificationService(VivelContext context, IMapper mapper) : base(context, mapper)
         {
         }
+
+        public async Task PostNotifications<T>(List<string> userIds, string linkId, string title, string content)
+        {
+            var notifications = userIds.Select(userId => new Notification
+            {
+                UserId = userId,
+                LinkId = linkId,
+                LinkType = typeof(T).Name,
+                Title = title,
+                Content = content,
+
+            });
+
+            await _context.AddRangeAsync(notifications);
+            await _context.SaveChangesAsync();
+        }
     }
 }
