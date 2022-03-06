8a42d057dcd877f57b36296e7c0d32b48ce5402c
Harun Sabljaković <harun.sabljakovic@edu.fit.ba>
2022-03-06 15:15:20 +0100

    insert donation for user

--- Vivel/Controllers/DonationController.cs
+++ Vivel/Controllers/DonationController.cs
@@ -1,4 +1,8 @@
-﻿using Vivel.Interfaces;
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Vivel.Interfaces;
 using Vivel.Model.Dto;
 using Vivel.Model.Requests.Donation;
 using Vivel.Model.Requests.Faq;
@@ -10,6 +14,21 @@ namespace Vivel.Controllers
     {
         public DonationController(IDonationService service) : base(service)
         {
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "user")]
+        public async override Task<ActionResult<DonationDTO>> Insert([FromBody] DonationInsertRequest request)
+        {
+            var user = HttpContext.User;
+
+            var userClaimValue = user.FindFirst(ClaimTypes.NameIdentifier).Value;
+            var isUser = user.IsInRole("user");
+
+            if (isUser && userClaimValue == request.UserId)
+                return await base.Insert(request);
+
+            return Unauthorized();
         }
     }
 }
