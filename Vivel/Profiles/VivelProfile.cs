c38e0d58d4cbb27a0e6767abef120a9e269a42df
Nedžad Alibegović <nedzad@nedzad.dev>
2022-03-19 02:29:57 +0100

    Update mappings

--- Vivel/Profiles/VivelProfile.cs
+++ Vivel/Profiles/VivelProfile.cs
@@ -16,28 +16,31 @@ namespace Vivel.Profiles
     {
         public VivelProfile()
         {
-            CreateMap<Database.Faq, FaqDTO>().ReverseMap();
-            CreateMap<Database.Faq, FaqInsertRequest>().ReverseMap();
-            CreateMap<Database.Faq, FaqUpdateRequest>().ReverseMap();
+            // Faq
+            CreateMap<Database.Faq, FaqDTO>();
+            CreateMap<FaqInsertRequest, Database.Faq>();
+            CreateMap<FaqUpdateRequest, Database.Faq>();
 
-            CreateMap<Database.PresetBadge, PresetBadgeDTO>().ReverseMap();
-            CreateMap<Database.PresetBadge, PresetBadgeUpsertRequest>().ReverseMap();
+            // PresetBadge
+            CreateMap<Database.PresetBadge, PresetBadgeDTO>();
+            CreateMap<PresetBadgeUpsertRequest, Database.PresetBadge>();
 
+            // Hospital
             CreateMap<Database.Hospital, HospitalDTO>()
                 .ForMember(destination => destination.Latitude, o => o.MapFrom(source => source.Location.Y))
-                .ForMember(destination => destination.Longitude, o => o.MapFrom(source => source.Location.X))
-                .ReverseMap();
+                .ForMember(destination => destination.Longitude, o => o.MapFrom(source => source.Location.X));
             CreateMap<HospitalUpsertRequest, Database.Hospital>()
                 .ForMember(destination => destination.Location, o => o.MapFrom(source => GeographyHelper.CreatePoint(source.Longitude, source.Latitude)));
 
-            CreateMap<Database.Notification, NotificationDTO>().ReverseMap();
-            CreateMap<Database.Notification, NotificationInsertRequest>().ReverseMap();
+            //  Notification
+            CreateMap<Database.Notification, NotificationDTO>();
+            CreateMap<NotificationInsertRequest, Database.Notification>();
 
+            // User
             CreateMap<Database.User, UserDTO>()
                 .ForMember(destination => destination.Latitude, o => o.MapFrom(source => source.Location.Y))
                 .ForMember(destination => destination.Longitude, o => o.MapFrom(source => source.Location.X))
-                .ForMember(destination => destination.BloodType, o => o.MapFrom(source => source.BloodType.Name))
-                .ReverseMap();
+                .ForMember(destination => destination.BloodType, o => o.MapFrom(source => source.BloodType.Name));
             CreateMap<Database.User, UserDetailsDTO>()
                 .ForMember(destination => destination.BloodType, o => o.MapFrom(source => source.BloodType.Name))
                 .ForMember(destination => destination.DonationCount, o => o.MapFrom(source => source.Donations.Count))
@@ -46,23 +49,23 @@ namespace Vivel.Profiles
             CreateMap<UserUpdateRequest, Database.User>()
                 .ForMember(destination => destination.Location, o => o.MapFrom(source => GeographyHelper.CreatePoint(source.Longitude, source.Latitude)));
 
+            // Drive
             CreateMap<Database.Drive, DriveDTO>()
                 .ForMember(destination => destination.BloodType, o => o.MapFrom(source => source.BloodType.Name))
-                .ForMember(destination => destination.Status, o => o.MapFrom(source => source.Status.Name))
-                .ReverseMap();
+                .ForMember(destination => destination.Status, o => o.MapFrom(source => source.Status.Name));
             CreateMap<Database.Drive, DriveDetailsDTO>()
                 .ForMember(destination => destination.BloodType, o => o.MapFrom(source => source.BloodType.Name))
                 .ForMember(destination => destination.AmountLeft, o => o.MapFrom(source => source.Amount - source.Donations.Where(x => x.Status.Name == "Approved").Sum(x => x.Amount)))
                 .ForMember(destination => destination.PendingCount, o => o.MapFrom(source => source.Donations.Where(x => x.Status.Name == "Pending").Count()))
                 .ForMember(destination => destination.ScheduledCount, o => o.MapFrom(source => source.Donations.Where(x => x.Status.Name == "Scheduled").Count()))
                 .ForMember(destination => destination.Status, o => o.MapFrom(source => source.Status.Name));
-            CreateMap<Database.Drive, DriveInsertRequest>()
-                .ForMember(destination => destination.BloodType, o => o.MapFrom(source => source.BloodType.Name))
-                .ReverseMap();
-            CreateMap<Database.Drive, DriveUpdateRequest>()
-                .ForMember(destination => destination.BloodType, o => o.MapFrom(source => source.BloodType.Name))
-                .ReverseMap();
+            CreateMap<DriveInsertRequest, Database.Drive>()
+                .ForPath(s => s.BloodType, opt => opt.Ignore());
+            CreateMap<DriveUpdateRequest, Database.Drive>()
+                .ForPath(s => s.Status, opt => opt.Ignore())
+                .ForPath(s => s.BloodType, opt => opt.Ignore());
 
+            // Donation
             CreateMap<Database.Donation, DonationDTO>()
                 .ForMember(destination => destination.UserName, o => o.MapFrom(source => source.User.UserName))
                 .ForMember(destination => destination.BloodType, o => o.MapFrom(source => source.Drive.BloodType.Name))
@@ -71,19 +74,17 @@ namespace Vivel.Profiles
                 .ForMember(destination => destination.ErythrocyteCount, o => o.MapFrom(source => source.DonationReport.ErythrocyteCount))
                 .ForMember(destination => destination.LeukocyteCount, o => o.MapFrom(source => source.DonationReport.LeukocyteCount))
                 .ForMember(destination => destination.PlateletCount, o => o.MapFrom(source => source.DonationReport.PlateletCount))
-                .ForMember(destination => destination.Note, o => o.MapFrom(source => source.DonationReport.Note))
-                .ReverseMap();
-            CreateMap<Database.Donation, DonationInsertRequest>()
-                .ReverseMap();
-            CreateMap<Database.Donation, DonationUpdateRequest>()
-                .ForMember(destination => destination.Status, o => o.MapFrom(source => source.Status.Name))
-                .ForMember(destination => destination.BloodType, o => o.MapFrom(source => source.Drive.BloodType.Name))
-                .ReverseMap()
-                .ForPath(s => s.Status.Name, opt => opt.Ignore());
+                .ForMember(destination => destination.Note, o => o.MapFrom(source => source.DonationReport.Note));
+            CreateMap<DonationInsertRequest, Database.Donation>();
+            CreateMap<DonationUpdateRequest, Database.Donation>()
+                .ForPath(s => s.Status, opt => opt.Ignore())
+                .ForPath(s => s.Drive.BloodType, opt => opt.Ignore());
 
-            CreateMap<Database.DonationReport, DonationUpdateRequest>().ReverseMap();
-            CreateMap<Database.DonationReport, DonationInsertRequest>().ReverseMap();
+            // DonationReport
+            CreateMap<DonationUpdateRequest, Database.DonationReport>();
+            CreateMap<DonationInsertRequest, Database.DonationReport>();
 
+            // Badge
             CreateMap<Database.Badge, BadgeDTO>()
                 .ForMember(destination => destination.Name, o => o.MapFrom(source => source.Name))
                 .ForMember(destination => destination.Description, o => o.MapFrom(source => source.PresetBadge.Description))
