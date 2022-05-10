7ecb2f79e93ffbbd39963ab0cdfa95fea8c3e5b4
Harun Sabljakovic <harun.sabljakovic@edu.fit.ba>
2022-05-10 12:50:18 +0200

    Remove unused methods

--- Vivel.Desktop/frmMain.cs
+++ Vivel.Desktop/frmMain.cs
@@ -35,10 +35,10 @@ namespace Vivel.Desktop
 
             _oidcClient = new OidcClient(options);
 
-            Login().GetAwaiter().OnCompleted(() => DisplayHospitalForm());
+            Login().GetAwaiter().OnCompleted(() => DisplayDefaultForm());
         }
 
-        private void DisplayHospitalForm()
+        private void DisplayDefaultForm()
         {
             menuStrip.Enabled = true;
 
@@ -93,51 +93,6 @@ namespace Vivel.Desktop
             if (saveFileDialog.ShowDialog(this) == DialogResult.OK)
             {
                 string FileName = saveFileDialog.FileName;
-            }
-        }
-
-        private void ExitToolsStripMenuItem_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void CutToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void CopyToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void PasteToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-        }
-
-        private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.Cascade);
-        }
-
-        private void TileVerticalToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.TileVertical);
-        }
-
-        private void TileHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.TileHorizontal);
-        }
-
-        private void ArrangeIconsToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            LayoutMdi(MdiLayout.ArrangeIcons);
-        }
-
-        private void CloseAllToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            foreach (Form childForm in MdiChildren)
-            {
-                childForm.Close();
             }
         }
 
